2a832a356ba857910d2d267f626339e308f2cb7f
codeteo <ramones.teo@gmail.com>
2019-03-18 20:44:53 +0200

    Removes Palette - adds scrim

--- app/src/main/java/com/assignment/teo/features/search/fragments/movies/adapter/MovieViewHolder.java
+++ app/src/main/java/com/assignment/teo/features/search/fragments/movies/adapter/MovieViewHolder.java
@@ -1,19 +1,15 @@
 package com.assignment.teo.features.search.fragments.movies.adapter;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.support.v7.graphics.Palette;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.assignment.teo.Constants;
 import com.assignment.teo.R;
 import com.assignment.teo.common.base.BaseViewHolder;
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
+
+import static com.assignment.teo.Constants.appendImageUrl;
 
 /**
  * View holder for the movie item.
@@ -26,9 +22,6 @@ public class MovieViewHolder extends BaseViewHolder<MovieViewModel> {
     private ImageView ivPoster;
 
     private final Context context;
-
-    // Needs a strong reference so it won't be garbage collected
-    private Target target;
 
     MovieViewHolder(View itemView) {
         super(itemView);
@@ -44,42 +37,10 @@ public class MovieViewHolder extends BaseViewHolder<MovieViewModel> {
     public void setViewModel(MovieViewModel viewModel) {
         tvTitle.setText(viewModel.getTitle());
 
-        target = getTarget();
-
         Picasso.with(context)
-                .load(Constants.appendImageUrl(viewModel.getThumbnail()))
-                .into(target);
-
+                .load(appendImageUrl(viewModel.getThumbnail()))
+                .fit()
+                .error(R.drawable.placeholder_movie)
+                .into(ivPoster);
     }
-
-    private Target getTarget() {
-        return new Target() {
-            @Override
-            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-//                ivPoster.setImageBitmap(null);
-                ivPoster.setImageBitmap(bitmap);
-
-                Palette.from(bitmap)
-                        .generate(palette -> {
-                            Palette.Swatch swatch = palette.getVibrantSwatch();
-                            int textColor = Color.WHITE;
-                            if (swatch != null) {
-                                textColor = swatch.getRgb();
-                            }
-                            tvTitle.setTextColor(textColor);
-                        });
-            }
-
-            @Override
-            public void onBitmapFailed(Drawable errorDrawable) {
-                ivPoster.setImageBitmap(null);
-            }
-
-            @Override
-            public void onPrepareLoad(Drawable placeHolderDrawable) {
-                ivPoster.setImageBitmap(null);
-            }
-        };
-    }
-
 }
--- app/src/main/java/com/assignment/teo/features/search/fragments/shows/adapter/ShowViewHolder.java
+++ app/src/main/java/com/assignment/teo/features/search/fragments/shows/adapter/ShowViewHolder.java
@@ -1,19 +1,15 @@
 package com.assignment.teo.features.search.fragments.shows.adapter;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.support.v7.graphics.Palette;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.assignment.teo.Constants;
 import com.assignment.teo.R;
 import com.assignment.teo.common.base.BaseViewHolder;
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
+
+import static com.assignment.teo.Constants.appendImageUrl;
 
 /**
  * View holder for the TV Show list item.
@@ -26,9 +22,6 @@ public class ShowViewHolder extends BaseViewHolder<ShowViewModel> {
     private ImageView ivPoster;
 
     private final Context context;
-
-    // Needs a strong reference so it won't be garbage collected
-    private Target target;
 
     ShowViewHolder(View itemView) {
         super(itemView);
@@ -44,43 +37,11 @@ public class ShowViewHolder extends BaseViewHolder<ShowViewModel> {
     public void setViewModel(ShowViewModel viewModel) {
         tvTitle.setText(viewModel.getTitle());
 
-        target = getTarget();
-
         Picasso.with(context)
-                .load(Constants.appendImageUrl(viewModel.getThumbnail()))
-                .into(target);
-
+                .load(appendImageUrl(viewModel.getThumbnail()))
+                .fit()
+                .error(R.drawable.placeholder_movie)
+                .into(ivPoster);
     }
-
-    private Target getTarget() {
-        return new Target() {
-            @Override
-            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                ivPoster.setImageBitmap(null);
-                ivPoster.setImageBitmap(bitmap);
-
-                Palette.from(bitmap)
-                        .generate(palette -> {
-                            Palette.Swatch swatch = palette.getVibrantSwatch();
-                            int textColor = Color.WHITE;
-                            if (swatch != null) {
-                                textColor = swatch.getRgb();
-                            }
-                            tvTitle.setTextColor(textColor);
-                        });
-            }
-
-            @Override
-            public void onBitmapFailed(Drawable errorDrawable) {
-                ivPoster.setImageBitmap(null);
-            }
-
-            @Override
-            public void onPrepareLoad(Drawable placeHolderDrawable) {
-                ivPoster.setImageBitmap(null);
-            }
-        };
-    }
-
 }
 
